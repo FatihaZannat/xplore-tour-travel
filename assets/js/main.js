02f8d40aa503e9e1a693d832faf077037aeb56b8
Fatiha Zannat <fatihazannat.n@gmail.com>
2025-04-17 08:41:50 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+(function ($){
+    'use strict'
+
+ /*--------------------------------------------------------------
+    Scripts initialization
+  --------------------------------------------------------------*/
+  $.exists = function (selector) {
+    return $(selector).length > 0;
+  };
+
+ 
+
+    $(function (){
+     dynamicBackground()
+     mainNav()
+
+        function mainNav() {
+          $('.cs_nav').append('<span class="cs_menu_toggle"><span></span></span>');
+          $('.menu-item-has-children').append(
+            '<span class="cs_munu_dropdown_toggle"><span></span></span>',
+          );
+          $('.cs_menu_toggle').on('click', function () {
+            $(this)
+              .toggleClass('cs_toggle_active')
+              .siblings('.cs_nav_list_wrap')
+              .toggleClass('cs_active');
+          });
+          $('.cs_munu_dropdown_toggle').on('click', function () {
+            $(this).toggleClass('active').siblings('ul').slideToggle();
+            $(this).parent().toggleClass('active');
+          });
+          // Search Toggle
+          $('.cs_search_tobble_btn').on('click', function () {
+            $('.cs_header_form_wrap').toggleClass('active');
+          });
+          $('.cs_header_form_overlay').on('click', function () {
+            $('.cs_header_form_wrap').removeClass('active');
+          });
+        }
+      
+       
+        function dynamicBackground() {
+            $('[data-src]').each(function () {
+              let src = $(this).attr('data-src');
+             
+              $(this).css({
+                'background-image': 'url(' + src + ')',
+              });
+              $(this).css('background-color', 'red');
+            });
+          }
+
+
+
+    })
+
+})(jQuery) // end of use strict
