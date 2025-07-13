698bdd73d33a944fdf050f3c9e5a6402fb4740cf
Fatiha Zannat <fatihazannat.n@gmail.com>
2025-07-13 23:17:44 +0600

    header color solve

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,10 +47,12 @@
           .toggleClass("cs_toggle_active")
           .siblings(".cs_nav_list_wrap")
           .toggleClass("cs_active");
+          stickyHeader();
       });
       $(".cs_munu_dropdown_toggle").on("click", function () {
         $(this).toggleClass("active").siblings("ul").slideToggle();
         $(this).parent().toggleClass("active");
+        
       });
       // Search Toggle
       $(".cs_search_tobble_btn").on("click", function () {
@@ -65,7 +67,8 @@
   --------------------------------------------------------------*/
     function stickyHeader() {
       var scroll = $(window).scrollTop();
-      if (scroll >= 10) {
+      var isMenuActive = $(".cs_nav_list_wrap").hasClass("cs_active")
+      if (scroll >= 10 || isMenuActive) {
         $(".cs_sticky_header").addClass("cs_sticky_active");
       } else {
         $(".cs_sticky_header").removeClass("cs_sticky_active");
